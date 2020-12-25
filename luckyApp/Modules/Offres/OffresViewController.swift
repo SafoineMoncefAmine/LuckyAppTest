d293c23bf80322df169146fc4110e4e7ca6ce042
Safoine Moncef Amine <nmsa@alphanetworks.be>
2020-12-25 22:56:40 +0100

    Add some explicatif comments.

--- luckyApp/Modules/Offres/OffresViewController.swift
+++ luckyApp/Modules/Offres/OffresViewController.swift
@@ -58,20 +58,20 @@ extension OffresViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let data = self.data else {return 0}
-        if section == 0 {
+        if section == 0 { // first section contains only the top header
             return 1
         } else {
-            return data.sections[section - 1].items.count + 1 // we add 1 for section header
+            return data.sections[section - 1].items.count + 1 // we add 1 for each section header
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
+        if indexPath.section == 0 { // this for top header
             return self.tableView(tableView, topHeaderCellForRowAt: indexPath, with: self.data?.offersCount)
-        } else if indexPath.row == 0 {
+        } else if indexPath.row == 0 { // this for section header
             let cellData = self.data?.sections[indexPath.section - 1].title
             return self.tableView(tableView, sectionHeaderCellForRowAt: indexPath, with: cellData)
-        } else {
+        } else { // this for offer cell
             let cellData = self.data?.sections[indexPath.section - 1].items[indexPath.row - 1]
             return self.tableView(tableView, offerViewCellForRowAt: indexPath, with: cellData)
         }
